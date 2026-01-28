9d14ef6f9cecc612333b78bbce9b2d841890e7cc
Himanshu Nishad <himanshunishad620@gmail.com>
2026-01-28 09:58:11 +0530

    fix : input validation

--- nexgenesis-assignment/src/helper/date.js
+++ nexgenesis-assignment/src/helper/date.js
@@ -2,20 +2,19 @@ const DATE_REGEX = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
 
 export const isValidDate = (dateStr) => {
   if (!DATE_REGEX.test(dateStr)) return false;
-
   const [day, month, year] = dateStr.split("/").map(Number);
-
-  const currentYear = new Date().getFullYear();
-
-  if (year > currentYear) return false;
-
-  const date = new Date(year, month - 1, day);
-
-  return (
-    date.getFullYear() === year &&
-    date.getMonth() === month - 1 &&
-    date.getDate() === day
-  );
+  const inputDate = new Date(year, month - 1, day);
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  if (
+    inputDate.getFullYear() !== year ||
+    inputDate.getMonth() !== month - 1 ||
+    inputDate.getDate() !== day
+  ) {
+    return false;
+  }
+  if (inputDate > today) return false;
+  return true;
 };
 
 export const formatMonthYear = (dateStr) => {
